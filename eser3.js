5f89e1c69b130236f72010579918f8eb60a2d92e
vittori02-ui <vittorioprivitera02@gmail.com>
2026-08-08 17:34:09 +0200

    the upgrade of the js script

--- eser3.js
+++ eser3.js
@@ -1,41 +1,38 @@
 function scelta() {
-    let dim=document.getElementById("dim").value;
-    let color=document.getElementById("color").value;
-    let testo=document.getElementById("txt").value;
+    let larghezza=document.getElementById("dim").value;
+    let altezza=document.getElementById("dim1").value;
+    let coloreSfondo=document.getElementById("col").value;
+    let testoDestra=document.getElementById("testoDestra").value;
+    let testoSinistra=document.getElementById("testoSinistra").value;
+    let testoCentro=document.getElementById("testoCentro").value;
+    let bordoArrotondato=document.getElementById("bordo1").checked;
+    let ombra=document.getElementById("bordo2").checked;
 
-    let sceltaCSS=document.querySelector('input[name="css"]:checked');
-
-    // Cancella eventuale div precedente
-    let box=document.getElementById("box");
+    let box=document.getElementById("box")
     box.innerHTML="";
-
-    // Crea la div
     let nuovaDiv=document.createElement("div");
-    // Imposta dimensioni e colore
-    nuovaDiv.style.width=dim+"px";
-    nuovaDiv.style.height=dim/2+"px";
-    nuovaDiv.style.backgroundColor=color;
-    /*
-    nuovaDiv.style.display="flex";
-    nuovaDiv.style.justifyContent="center";
-    nuovaDiv.style.alignItems="center";*/
+    nuovaDiv.style.width=larghezza+"px";
+    nuovaDiv.style.height=lunghezza+"px";
+    nuovaDiv.style.backgroundColor=coloreSfondo;
 
-    // Applica lo stile scelto
-    if(sceltaCSS)
-    {
-        if(sceltaCSS.value==="bordo")
-        {
-            nuovaDiv.style.border="3px solid black";
-        }
-        if(sceltaCSS.value==="tondo") 
-        {
-            nuovaDiv.style.borderRadius="20px";
-        }
-        if(sceltaCSS.value==="ombra") 
-        {
-            nuovaDiv.style.boxShadow="5px 5px 10px gray";
-        }
-    }
-    nuovaDiv.textContent = testo;
+    if(bordoArrotondato)nuovaDiv.style.borderRadius="20px";
+    if(ombra)nuovaDiv.style.boxShadow="5px 5px 10px gray";
+    nuovaDiv.style.display="flex";
+    nuovaDiv.style.justifyContent="space-between";
+    nuovaDiv.style.alignItems="center";
+    nuovaDiv.style.padding="10px";
+    nuovaDiv.style.fontFamily="Verdana";
+
+    let divSinistra=document.createElement("div");
+    divSinistra.textContent=testoSinistra;
+    let divCentro=document.createElement("div");
+    divCentro.textContent=testoCentro;
+    divCentro.style.textAlign="center";
+    divCentro.style.flexGrow="1";
+    let divDestra=document.createElement("div");
+    divDestra.textContent=testoDestra;
+    nuovaDiv.appendChild(divSinistra);
+    nuovaDiv.appendChild(divCentro);
+    nuovaDiv.appendChild(divDestra);
     box.appendChild(nuovaDiv);
 }
